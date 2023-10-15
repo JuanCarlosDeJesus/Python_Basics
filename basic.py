39ffe8203e9792132c4afe830e6e82f8cc7f5dfd
Juan Carlos De Jesus <carl.d3j3sus@gmail.com>
2023-10-15 08:09:31 +0800

    Added basic.py

---
+++ basic.py
@@ -0,0 +1,7 @@
+# using input
+print("You are targeting 10.10.10.10" + input("What ip would you like to target? "))
+
+# take first name as and input and last name as an input and display in console "Your name is ___ ____"
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+print(f"Your name is {fname} {lname}.")
