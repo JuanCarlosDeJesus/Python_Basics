e238bfaf96c047a5886adf4653a05ff4d14b78ba
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2026-04-11 17:04:30 +0800

    Array

---
+++ Array/array.py
@@ -0,0 +1,22 @@
+"""
+Array -
+linear data structure
+contiguous memory allocation
+access elements using index
+homogeneous data type
+why?
+suitable for storing and manipulating large amounts of data
+efficient access and retrieval of elements
+supports various operations like insertion, deletion, searching, sorting
+kinds?
+1D array
+can be a row
+one index used
+  ex. int arr[5] or int arr[] = {1, 2, 3, 4, 5}
+2D array
+can be a table
+two indices used
+multi-dimensional array
+can be a cube or higher-dimensional structure
+multiple indices used
+"""
