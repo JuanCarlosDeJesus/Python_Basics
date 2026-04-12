7020b0e59d3a9968c1ab31fa45aa8853b018dda0
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2026-04-12 11:50:22 +0800

    1D array

--- Array/array.py
+++ Array/array.py
@@ -4,15 +4,21 @@ linear data structure
 contiguous memory allocation
 access elements using index
 homogeneous data type
-why?
+use for -
 suitable for storing and manipulating large amounts of data
 efficient access and retrieval of elements
 supports various operations like insertion, deletion, searching, sorting
-kinds?
+"""
+"""
 1D array
 can be a row
 one index used
+Declaration - and initialization of 1D array
   ex. int arr[5] or int arr[] = {1, 2, 3, 4, 5}
+"""
+
+
+"""  
 2D array
 can be a table
 two indices used
