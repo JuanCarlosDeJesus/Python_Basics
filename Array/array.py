43ba44b8b9860ab4bed10a566289a9b54eb8b61f
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2026-04-12 17:03:08 +0800

    2D Array

--- Array/array.py
+++ Array/array.py
@@ -16,13 +16,36 @@ one index used
 Declaration - and initialization of 1D array
   ex. int arr[5] or int arr[] = {1, 2, 3, 4, 5}
 """
+print("How many elements do you want to store in the array?", end=" ")
+num = input()
+arr = []
 
+for i in range(int(num)):
+    print(f"Enter {num} elements:", end="")
+    element = input()
+    arr.append(element)
+print("The array is:", arr)
+for i in range(int(num)):
+    print(f"Element at index {i} is: {arr[i]}")
 
 """  
 2D array
 can be a table
 two indices used
+"""
+r_num = int(input("Enter the number of rows: "))
+c_num = int(input("Enter the number of columns: "))
+two_d_array = [[0 for col in range(c_num)] for row in range(r_num)]
+
+for row in range(r_num):
+    for col in range(c_num):
+        # print(f"Enter element for row {i} and column {col}:", end="")
+        two_d_array[row][col] = row * col
+print(f"The 2D array is: {two_d_array}")
+
+"""
 multi-dimensional array
 can be a cube or higher-dimensional structure
 multiple indices used
 """
+
