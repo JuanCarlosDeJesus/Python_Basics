e601e936f37c2d4684eda84b03b0859d6b0216fb
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2026-04-13 16:13:10 +0800

    Del, Sort, Search Array

--- Array/array.py
+++ Array/array.py
@@ -49,3 +49,56 @@ can be a cube or higher-dimensional structure
 multiple indices used
 """
 
+# Delete elements from the array
+print("Enter the size of the array:", end="")
+tot = int(input())
+arr = []
+print(end=f"Enter {tot} elements: ")
+for i in range(tot):
+    arr.append(input())
+
+print(end="\nEnter the value to delete: ")
+val = input()
+
+if val in arr:
+    arr.remove(val)
+    print("\nThe New Array is: ")
+    for i in range(tot - 1):
+        print(end=arr[i]+" ")
+else:
+    print("\nValue not found in the array.")
+
+
+# Sort the array
+arr = [10,22,38,27,11]
+temp = 0
+
+# Displaying the unsorted array
+print("The unsorted array is: ", end="")
+for i in range(0, len(arr)):
+    print(arr[i], end=" ")
+# Sorting the array in ascending order
+for i in range(0, len(arr)):
+    for j in range(i+1, len(arr)):
+        if arr[i] > arr[j]:
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+print()
+# Displaying the sorted array
+print("The sorted array is: ", end="")
+for i in range(0, len(arr)):
+    print(arr[i], end=" ")
+
+# Search for an element in the array
+import array
+
+arr = array.array('i', [1, 2, 3, 1, 2, 5])
+print("The newly created array is: ", end="")
+for i in range(0, 6):
+    print(arr[i], end=" ")
+print("\r")
+print("The index of the first occurrence of 2 is: ", end="")
+print(arr.index(2))
+print("The index of the first occurrence of 1 is: ", end="")
+print(arr.index(1))
