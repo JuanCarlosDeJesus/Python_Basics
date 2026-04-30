b5c03a014b5c2147748c3d21e539ce11eb6457ca
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2026-04-30 15:24:24 +0800

    Decorators complete

--- Decorators/deco.py
+++ Decorators/deco.py
@@ -55,4 +55,44 @@ def hello(name):
 
 print(hello("Alice"))
 
+# Using decorators to log
+def logged(function):
+    def wrapper(*args, **kwargs):
+        value = function(*args, **kwargs)
+        with open('logfile.txt', 'a+') as f:
+            fname = function.__name__
+            print(f"{fname} returned value {value}")
+            f.write(f"{fname} returned value {value}\n")
+        return value
+
+    return wrapper
+
+@logged
+def add(x,y):
+    return x+y
+
+print(add(10,20))
+
+# Timing functions with decorators
+import time
+
+def timed(function):
+    def wrapper(*args, **kwargs):
+        before = time.time()
+        value = function(*args, **kwargs)
+        after = time.time()
+        fname = function.__name__
+        print(f"{fname} took {after-before} seconds")
+        return value
+    return wrapper
+
+@timed
+def myfunc(x):
+    result = 1
+    for i in range(1,x):
+        result *= i
+    return result
+
+myfunc(1000)
+
 
