ef5c952def335fd93f5e4d38c6bb6d99eaae4bf5
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2026-05-01 15:35:45 +0800

    Generators

--- Generators/gen.py
+++ Generators/gen.py
@@ -1,1 +1,21 @@
 # Generators
+import sys 
+
+def mygen(x):
+    for x in range(x):
+        yield x ** 3
+
+values = mygen(10000)
+
+print(sys.getsizeof(values))
+
+# Example 2
+def infinite_sequence():
+    result = 1
+    while True:
+        yield result
+        result *= 5
+
+values = infinite_sequence()
+# print(next(values))
+# print(next(values))
